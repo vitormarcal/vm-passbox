66a06c840ce63c776d51b0f92939899494d3fafe
Vítor <vitormarcal8@gmail.com>
2017-03-28 17:26:14 -0300

    Adicionado LocalCredenciadoController

---
+++ src/main/java/com/vitormarcal/passbox/controller/LocalCredenciadoController.java
@@ -0,0 +1,34 @@
+package com.vitormarcal.passbox.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.vitormarcal.passbox.model.LocalCredenciado;
+import com.vitormarcal.passbox.repository.LocaisCredenciados;
+
+@Controller
+@RequestMapping("/locaisCredenciados")
+public class LocalCredenciadoController {
+
+	@Autowired
+	private LocaisCredenciados locaisCredenciados;
+	
+	@GetMapping
+	public ModelAndView listar(){
+		ModelAndView modelAndView = new ModelAndView("ListaLocaisCredenciados");
+		
+		modelAndView.addObject("locaisCredenciados", locaisCredenciados.findAll());
+		modelAndView.addObject("localCredenciado", new LocalCredenciado());
+		return modelAndView;
+	}
+	
+	@PostMapping
+	public String salvar(LocalCredenciado localCredenciado){
+		this.locaisCredenciados.save(localCredenciado);
+		return "redirect:/locaisCredenciados";
+	}
+}
