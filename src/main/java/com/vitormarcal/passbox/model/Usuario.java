a1b8f94d9863043b13ccef30bae85fbd651daa3f
Vítor Marçal <git@vitormarcal.com.br>
2017-03-28 08:50:16 -0300

    Acrescentado mapeamentos e mudanças de nomes nas classes e atributos

---
+++ src/main/java/com/vitormarcal/passbox/model/Usuario.java
@@ -0,0 +1,69 @@
+package com.vitormarcal.passbox.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	@Column(name = "idUsuario")
+	private Long id;
+	
+	private String userName;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "idPessoa", nullable = false)
+	private Pessoa pessoa;
+	
+	@Transient
+	private List<Senha> senhas;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	
+	public List<Senha> getSenhas() {
+		return senhas;
+	}
+	public void setSenhas(List<Senha> senhas) {
+		this.senhas = senhas;
+	}
+
+	
+}
