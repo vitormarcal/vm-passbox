6ef4c0ac882a1a3ac97de6f02a26b32524ce0521
Vítor <vitormarcal8@gmail.com>
2017-04-07 15:07:33 -0300

    Separado Em views diferentes a ação de listar e de incluir 'model'

--- src/main/java/com/vitormarcal/passbox/controller/LocaisCredenciadosController.java
+++ src/main/java/com/vitormarcal/passbox/controller/LocaisCredenciadosController.java
@@ -3,6 +3,7 @@ package com.vitormarcal.passbox.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,7 +23,12 @@ public class LocaisCredenciadosController {
 		ModelAndView modelAndView = new ModelAndView("ListaLocaisCredenciados");
 		
 		modelAndView.addObject("locaisCredenciados", locaisCredenciados.findAll());
-		modelAndView.addObject("localCredenciado", new LocalCredenciado());
+		return modelAndView;
+	}
+	
+	@GetMapping("/novo")
+	public ModelAndView novo() {
+		ModelAndView modelAndView = new ModelAndView("CadastroLocalCredenciado");
 		return modelAndView;
 	}
 	
@@ -30,5 +36,10 @@ public class LocaisCredenciadosController {
 	public String salvar(LocalCredenciado localCredenciado){
 		this.locaisCredenciados.save(localCredenciado);
 		return "redirect:/locaisCredenciados";
+	}
+	
+	@ModelAttribute("localCredenciado")
+	public LocalCredenciado getPessoa(){
+		return new LocalCredenciado();
 	}
 }
