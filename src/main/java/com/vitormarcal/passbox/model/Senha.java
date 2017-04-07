2f477e2e2abb29669c1349a10e15e8df18c37c0e
Vítor <vitormarcal8@gmail.com>
2017-04-07 14:11:30 -0300

    Mundançãs no Locale, criação de pagina de usuario

--- src/main/java/com/vitormarcal/passbox/model/Senha.java
+++ src/main/java/com/vitormarcal/passbox/model/Senha.java
@@ -65,5 +65,41 @@ public class Senha implements Serializable {
 	}
 	
 	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((palavraPasse == null) ? 0 : palavraPasse.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Senha other = (Senha) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (palavraPasse == null) {
+			if (other.palavraPasse != null)
+				return false;
+		} else if (!palavraPasse.equals(other.palavraPasse))
+			return false;
+		return true;
+	}
+
+
+	
+	
+	
+	
 
 }
