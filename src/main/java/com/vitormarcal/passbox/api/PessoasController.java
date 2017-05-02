197ad6c83e55245637f5693afa6e902ccb5e090e
Vítor Marçal <git@vitormarcal.com.br>
2017-05-02 13:52:36 -0300

    update

--- src/main/java/com/vitormarcal/passbox/api/PessoasController.java
+++ src/main/java/com/vitormarcal/passbox/api/PessoasController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vitormarcal.passbox.model.Pessoa;
-import com.vitormarcal.passbox.model.Usuario;
 import com.vitormarcal.passbox.service.PessoaService;
 
 @RestController
--- src/main/java/com/vitormarcal/passbox/repository/Usuarios.java
+++ src/main/java/com/vitormarcal/passbox/repository/Usuarios.java
@@ -1,12 +1,9 @@
 package com.vitormarcal.passbox.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vitormarcal.passbox.model.Usuario;
 
 public interface Usuarios extends JpaRepository<Usuario, Long> {
-
 
 }
